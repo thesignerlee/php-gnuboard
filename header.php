0acb05200a3111de8ac5a7c6552b8981104f00de
jongjealee <jongjealee@naver.com>
2023-04-21 16:34:20 +0900

    main

---
+++ header.php
@@ -0,0 +1,9 @@
+<header>
+    <h1><a href="./index.html"><img src="#" alt="네이버"></a></h1>
+    <nav>
+        <a href="./mail.html">메일</a>
+        <a href="./cafe.html">카페</a>
+        <a href="#">블로그</a>
+    </nav>
+</header>
+<!-- 반드시 모든 작업 끝마치고 PHP에 옮길 것 -->
